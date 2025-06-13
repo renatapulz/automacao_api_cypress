ca9510fbe895bb36824e42e38b055bdf2647334e
Renata Pulz <renatadpulz@gmail.com>
2025-06-12 21:02:37 -0300

    inserido teste pra listar os usuarios e retornar 10

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -14,5 +14,18 @@ describe("Teste API's", () => {
           expect(response.body).to.have.property('accessToken')
           cy.log('AccessToken:', response.body.accessToken)
         })
-      })
+    })
+
+    it('Listagem de todos os usuários', () => {
+        cy.request({
+          method: 'GET',
+          url: 'https://dummyjson.com/users?limit=10'
+        }).then((response) => {
+          expect(response.status).to.eq(200)
+          expect(response.body).to.have.property('users')
+          expect(response.body.users).to.be.an('array').and.not.be.empty
+          cy.log('users:', JSON.stringify(response.body.users, null, 2))
+        })
+    })
+
 })
